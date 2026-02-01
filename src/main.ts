ff1896448209151353db25939f522ead2adeef76
Débora Andrade <deboraellenandrade@gmail.com>
2026-02-01 18:11:24 +0000

    feat: Atualiza a função renderApp para incluir informações de configuração do sistema e executa mainTest

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 import './style.css';
 import { renderUsers, loadInitialData } from './ui/RenderUser';
 import { renderTasks } from './ui/RenderTask';
+import { SystemConfig } from './services';
+import { mainTest } from './test';
 
 function renderApp() {
     
@@ -9,51 +11,55 @@ function renderApp() {
     renderTasks();
     renderUsers();
 
+console.log("System Config AppName, Version e Environment:", SystemConfig.getInfo());
 
-abstract class Box<T> {
-    abstract getItem(): T;
-};
+// abstract class Box<T> {
+//     abstract getItem(): T;
+// };
+// class StringBox extends Box<string> {
+//     getItem(): string {
+//         return "This is a string";
+//     }
+// };
 
-class StringBox extends Box<string> {
-    getItem(): string {
-        return "This is a string";
-    }
-};
+// // const myBox: Box<string> = new StringBox();
+// // console.log(myBox.getItem().toFixed(2));
 
-// const myBox: Box<string> = new StringBox();
-// console.log(myBox.getItem().toFixed(2));
+// const myBox2: Box<string> = new StringBox();
+// console.log(myBox2.getItem().toUpperCase());
 
-const myBox2: Box<string> = new StringBox();
-console.log(myBox2.getItem().toUpperCase());
+// class NumberBox extends Box<number> {
+//     getItem(): number {
+//         return 42;
+//     }
+// };
 
-class NumberBox extends Box<number> {
-    getItem(): number {
-        return 42;
-    }
-};
+// const myNumberBox: Box<number> = new NumberBox();
+// console.log(myNumberBox.getItem().toFixed(2));
 
-const myNumberBox: Box<number> = new NumberBox();
-console.log(myNumberBox.getItem().toFixed(2));
+// // const myNumberBox2: Box<number> = new NumberBox();
+// // console.log(myNumberBox2.getItem().toUpperCase());
 
-// const myNumberBox2: Box<number> = new NumberBox();
-// console.log(myNumberBox2.getItem().toUpperCase());
+// function logItem<T>(item: T): void {
+//     console.log("Item:", item);
+// }
 
-function logItem<T>(item: T): void {
-    console.log("Item:", item);
-}
+// logItem<string>("Hello, genérico!");
+// logItem<number>(12345);
 
-logItem<string>("Hello, genérico!");
-logItem<number>(12345);
+// class BoxAny {
+//     getitem(): any {
+//         return "This is any";
+//     }
+// };
 
-class BoxAny {
-    getitem(): any {
-        return "This is any";
-    }
-};
+// const myAnyBox: BoxAny = new BoxAny();
+// console.log(myAnyBox.getitem().toFixed(2));
+// console.log(myAnyBox.getitem().toUpperCase());
 
-const myAnyBox: BoxAny = new BoxAny();
-console.log(myAnyBox.getitem().toFixed(2));
-console.log(myAnyBox.getitem().toUpperCase());
+console.log("Running mainTest...");
+
+mainTest();
     
 }
 
